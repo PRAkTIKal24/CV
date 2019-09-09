52b1af40edddfb018654465ada46b004d66d1689
Pratik Jawahar <pratik.jawahar24@gmail.com>
2019-09-09 14:48:44 -0400

    ch

--- Binarization_50%.py
+++ Binarization_50%.py
@@ -2,22 +2,53 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-I = cv2.imread('hw0.jpeg')
-cv2.namedWindow('hw0',cv2.WINDOW_AUTOSIZE)
-cv2.imshow('',I)
+#read image in greyscale for thresholding
+I = cv2.imread('hwf.jpeg',0)
+cv2.namedWindow('hw02',cv2.WINDOW_AUTOSIZE)
+cv2.imshow('Original',I)
 cv2.waitKey(0)
-r, c, cl = I.shape
+r, c= I.shape
 Sum = 0
 
+#Calculate the histogram as
+hist = np.zeros(I.shape)
+hist = cv2.calcHist([I], [0], None, [256], [0, 256])
+print (np.max(hist))
+
+#normalize the histogram
+cv2.normalize(hist, hist)
+print (hist.shape)
+#plt.plot(hist)
+
+#calculate half area unde the histogram
+total_area = np.sum(hist[0:256])
+half_area = total_area/2
+
+flag = 1
+#Calculate a threshold based on 50% of the area
+for i in range(256):
+# 	area = np.sum(hist[0:i])
+# 	if (np.abs(area-half_area)<100000):
+# 		T = i
+# 	else:
+	a1 = np.sum(hist[0:i])
+	a2 = np.sum(hist[i:256])
+	if (np.abs(a2-a1)<0.02) and flag==1:
+		T = i
+		flag = 0
+
+print (T)
+						
+#Applying the threshold to the image to binarize
 for i in range(r):
 	for j in range(c):
-		if i%2 != 0 and j%2 ==0:
+		if ((I[i,j])>T):
 			I[i,j] = 255
 		else:
 			I[i,j] = 0
 
 
-cv2.namedWindow('hw0',cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('hw02',cv2.WINDOW_AUTOSIZE)
 cv2.imshow('',I)
 cv2.waitKey(0)
 			
--- Binarization_avgIntensity.py
+++ Binarization_avgIntensity.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-I = cv2.imread('hw0.jpeg')
-cv2.namedWindow('hw0',cv2.WINDOW_AUTOSIZE)
+I = cv2.imread('hwf.jpeg')
+cv2.namedWindow('hwf',cv2.WINDOW_AUTOSIZE)
 cv2.imshow('',I)
 cv2.waitKey(0)
 r, c, cl = I.shape
@@ -11,13 +11,13 @@ Sum = 0
 
 for i in range(r):
 	for j in range(c):
-		Sum += I[i,j,1]+I[i,j,2]+I[i,j,0]
+		Sum += ((I[i,j,1]+I[i,j,2]+I[i,j,0])**2)
 #Finding mean of all pixel intensities and setting it as the threshold
-T_avg = Sum/(r*c*cl)
+T_avg = np.log(((Sum)/(r*c*cl))**0.5)
 
 for i in range(r):
 	for j in range(c):
-		if (I[i,j,1]+I[i,j,2]+I[i,j,0])>T_avg:
+		if ((I[i,j,1]+I[i,j,2]+I[i,j,0])>T_avg):
 			I[i,j] = 0
 		else:
 			I[i,j] = 255
